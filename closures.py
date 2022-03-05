debe392a5150dc592c7176ceee8da5a37c360d16
Jairo Perez <jairo85cd@gmail.com>
2022-03-05 13:18:31 -0500

    CLosures modificado

--- closures.py
+++ closures.py
@@ -1,5 +1,12 @@
 #Hola 3-> HolaHolaHola
 
+
+def make_division_by(n):
+    def division_by(d):
+        return d/n
+    return division_by
+
+
 def make_repeater_of(n):
     def repeater(string):
         assert type(string) == str, "Solo puedes utilizar cadenas"
@@ -11,6 +18,10 @@ def run():
     print(repeate_5("Hola"))
     repeate_10= make_repeater_of(10)
     print(repeate_10("Jairo"))
+    division_by_3=make_division_by(3)
+    print(division_by_3(18))
+    division_by_5=make_division_by(5)
+    print(division_by_5(100))
 
 
 if __name__ =="__main__":
